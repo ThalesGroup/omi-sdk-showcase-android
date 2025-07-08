7e30949ababb1e0b01c58d759365c60912d6994a
Piotr Klis <piotr.klis@onewelcome.com>
2025-07-08 12:05:49 +0200

    AOSA-18 WIP

--- app/src/main/java/com/onewelcome/showcaseapp/feature/userauthentication/pinauthentication/PinAuthenticationScreen.kt
+++ app/src/main/java/com/onewelcome/showcaseapp/feature/userauthentication/pinauthentication/PinAuthenticationScreen.kt
@@ -2,22 +2,32 @@ package com.onewelcome.showcaseapp.feature.userauthentication.pinauthentication
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.onewelcome.core.components.ShowcaseFeatureDescription
 import com.onewelcome.core.components.ShowcaseTopBar
 import com.onewelcome.core.theme.Dimensions
 import com.onewelcome.showcaseapp.R
+import com.onewelcome.showcaseapp.feature.userregistration.browserregistration.BrowserRegistrationViewModel
+import com.onewelcome.showcaseapp.feature.userregistration.browserregistration.BrowserRegistrationViewModel.UiEvent
 
 @Composable
-fun PinAuthenticationScreen(navController: NavController) {
+fun PinAuthenticationScreen(
+  navController: NavController,
+  viewModel: BrowserRegistrationViewModel = hiltViewModel()
+) {
   PinAuthenticationScreenContent(
-    onNavigateBack = { navController.popBackStack() }
+    onNavigateBack = { navController.popBackStack() },
+    viewModel =
   )
 }
 
@@ -35,7 +45,15 @@ fun PinAuthenticationScreenContent(onNavigateBack: () -> Unit) {
       verticalArrangement = Arrangement.spacedBy(Dimensions.verticalSpacing)
     ) {
       ShowcaseFeatureDescription("Add some nice description", "")
-      Text("Pin authenticate me")
+      Button(
+        modifier = Modifier
+          .fillMaxWidth()
+          .height(Dimensions.actionButtonHeight),
+        onClick = { onEvent(UiEvent.CancelRegistration) },
+        enabled = isPinAuthenticationButtonEnabled
+      ) {
+        Text(stringResource(R.string.cancel_registration))
+      }
     }
   }
 }
