0495e04d6b45601fc13bdedf39057c2bf2102076
Piotr Klis <piotr.klis@onewelcome.com>
2025-07-14 14:23:43 +0200

    AOSA-18 initial screen scaffold

--- app/src/main/java/com/onewelcome/showcaseapp/feature/userauthentication/pinauthentication/PinAuthenticationScreen.kt
+++ app/src/main/java/com/onewelcome/showcaseapp/feature/userauthentication/pinauthentication/PinAuthenticationScreen.kt
@@ -13,26 +13,36 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.github.michaelbull.result.Result
+import com.github.michaelbull.result.onFailure
+import com.github.michaelbull.result.onSuccess
+import com.onewelcome.core.components.SdkFeatureScreen
 import com.onewelcome.core.components.ShowcaseFeatureDescription
 import com.onewelcome.core.components.ShowcaseTopBar
 import com.onewelcome.core.theme.Dimensions
 import com.onewelcome.showcaseapp.R
-import com.onewelcome.showcaseapp.feature.userregistration.browserregistration.BrowserRegistrationViewModel
-import com.onewelcome.showcaseapp.feature.userregistration.browserregistration.BrowserRegistrationViewModel.UiEvent
+import com.onewelcome.showcaseapp.feature.userauthentication.pinauthentication.PinAuthenticationViewModel.UiEvent
 
 @Composable
 fun PinAuthenticationScreen(
   navController: NavController,
-  viewModel: BrowserRegistrationViewModel = hiltViewModel()
+  viewModel: PinAuthenticationViewModel = hiltViewModel()
 ) {
   PinAuthenticationScreenContent(
+    uiState = viewModel.uiState,
     onNavigateBack = { navController.popBackStack() },
-    viewModel =
+    viewModel = viewModel,
+    onEvent = { viewModel.onEvent(it) },
   )
 }
 
 @Composable
-fun PinAuthenticationScreenContent(onNavigateBack: () -> Unit) {
+private fun PinAuthenticationScreenContent(
+  onNavigateBack: () -> Unit,
+  viewModel: PinAuthenticationViewModel,
+  uiState: PinAuthenticationViewModel.State,
+  onEvent: (UiEvent) -> Unit,
+) {
   Scaffold(
     topBar = {
       ShowcaseTopBar(stringResource(R.string.user_registration)) { onNavigateBack.invoke() }
@@ -44,16 +54,46 @@ fun PinAuthenticationScreenContent(onNavigateBack: () -> Unit) {
         .padding(start = Dimensions.mPadding, end = Dimensions.mPadding),
       verticalArrangement = Arrangement.spacedBy(Dimensions.verticalSpacing)
     ) {
-      ShowcaseFeatureDescription("Add some nice description", "")
+      SdkFeatureScreen(
+        title = stringResource(R.string.pin_authentication),
+        onNavigateBack = onNavigateBack,
+        description = {
+          ShowcaseFeatureDescription(
+            description = "Add some nice description",
+            link = "add a link"
+          )
+        },
+        settings = { SettingSection() },
+        result = uiState.result?.let { { PinAuthenticationResult(it) } }
+      ) { }
       Button(
         modifier = Modifier
           .fillMaxWidth()
           .height(Dimensions.actionButtonHeight),
-        onClick = { onEvent(UiEvent.CancelRegistration) },
-        enabled = isPinAuthenticationButtonEnabled
+        onClick = {}
+//        onClick = { onEvent(UiEvent.CancelRegistration) },
+//        enabled = isPinAuthenticationButtonEnabled
       ) {
         Text(stringResource(R.string.cancel_registration))
       }
     }
+  }
+}
+
+@Composable
+private fun SettingSection() {
+  TODO("Not yet implemented")
+}
+
+@Composable
+private fun PinAuthenticationResult(result: Result<Void, Throwable>?) {
+  Column {
+    result
+      ?.onSuccess {
+        Column {
+          Text("magnificent success")
+        }
+      }
+      ?.onFailure { Text("$it") }
   }
 }
---
+++ app/src/main/java/com/onewelcome/showcaseapp/feature/userauthentication/pinauthentication/PinAuthneticationViewModel.kt
@@ -0,0 +1,25 @@
+package com.onewelcome.showcaseapp.feature.userauthentication.pinauthentication
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.github.michaelbull.result.Result
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class PinAuthenticationViewModel @Inject() constructor() : ViewModel() {
+  var uiState by mutableStateOf(State())
+    private set
+
+  fun onEvent(value: UiEvent) {
+
+  }
+
+  data class State(val result: Result<Void, Throwable>? = null)
+
+  sealed interface UiEvent {
+    data object StartPinAuthentication : UiEvent
+  }
+}
