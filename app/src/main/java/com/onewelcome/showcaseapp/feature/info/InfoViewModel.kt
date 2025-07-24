5190a65c2d8d718a5c0f21ad754fd4e254215819
Piotr Klis <piotr.klis@onewelcome.com>
2025-07-24 16:22:21 +0200

    AOSA-18 refactored info screen & viewmodel to get rid of Result

--- app/src/main/java/com/onewelcome/showcaseapp/feature/info/InfoViewModel.kt
+++ app/src/main/java/com/onewelcome/showcaseapp/feature/info/InfoViewModel.kt
@@ -5,9 +5,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.github.michaelbull.result.Err
-import com.github.michaelbull.result.Ok
-import com.github.michaelbull.result.Result
 import com.github.michaelbull.result.onFailure
 import com.github.michaelbull.result.onSuccess
 import com.onewelcome.core.usecase.GetAuthenticatedUserProfileUseCase
@@ -37,14 +34,14 @@ class InfoViewModel @Inject constructor(
 
   private suspend fun updateUserProfiles() {
     getUserProfilesUseCase.execute()
-      .onSuccess { uiState = uiState.copy(userProfileIds = Ok(it.map { it.profileId }.toList())) }
-      .onFailure { uiState = uiState.copy(userProfileIds = Err(Unit)) }
+      .onSuccess { uiState = uiState.copy(userProfileIds = it.map { it.profileId }.toList()) }
+      .onFailure { uiState = uiState.copy(userProfileIds = emptyList()) }
   }
 
   private fun updateAuthenticatedUserProfile() {
     getAuthenticatedUserProfileUseCase.execute()
-      .onSuccess { uiState = uiState.copy(authenticatedUserProfileId = Ok(it?.profileId)) }
-      .onFailure { uiState = uiState.copy(authenticatedUserProfileId = Err(Unit)) }
+      .onSuccess { uiState = uiState.copy(authenticatedUserProfileId = it?.profileId ?: "") }
+      .onFailure { uiState = uiState.copy(authenticatedUserProfileId = "") }
   }
 
   private fun updateIsSdkInitialized() {
@@ -53,7 +50,7 @@ class InfoViewModel @Inject constructor(
 
   data class State(
     val isSdkInitialized: Boolean = false,
-    val userProfileIds: Result<List<String>, Unit>? = null,
-    val authenticatedUserProfileId: Result<String?, Unit>? = null,
+    val userProfileIds: List<String> = emptyList(),
+    val authenticatedUserProfileId: String = "",
   )
 }
