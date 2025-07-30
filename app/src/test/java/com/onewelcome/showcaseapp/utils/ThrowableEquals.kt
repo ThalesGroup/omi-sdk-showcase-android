4b22e076754073e97244df013fc293f2858056cd
Aleksander Nowakowski <aleksander.nowakowski@onegini.com>
2025-07-30 15:33:47 +0200

    AOSA-12 Apply review remarks

--- app/src/test/java/com/onewelcome/showcaseapp/utils/ThrowableEquals.kt
+++ app/src/test/java/com/onewelcome/showcaseapp/utils/ThrowableEquals.kt
@@ -1,6 +1,11 @@
 package com.onewelcome.showcaseapp.utils
 
+import org.assertj.core.api.RecursiveComparisonAssert
 import java.util.function.BiPredicate
+
+fun RecursiveComparisonAssert<*>.withEqualsForThrowable(): RecursiveComparisonAssert<*> {
+  return this.withEqualsForType(ThrowableEquals(), Throwable::class.java)
+}
 
 class ThrowableEquals : BiPredicate<Throwable, Throwable> {
 
