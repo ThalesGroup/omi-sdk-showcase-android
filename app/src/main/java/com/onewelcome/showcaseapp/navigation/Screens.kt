d21eecfff604a839f853038597607ba3b339f323
Piotr Klis <piotr.klis@onewelcome.com>
2025-09-11 12:02:46 +0200

    AOSA-17 added transaction result screen and connected it with transaction details screen with common viewmodel and navigation

--- app/src/main/java/com/onewelcome/showcaseapp/navigation/Screens.kt
+++ app/src/main/java/com/onewelcome/showcaseapp/navigation/Screens.kt
@@ -1,8 +1,10 @@
 package com.onewelcome.showcaseapp.navigation
 
 import com.onewelcome.core.util.Constants
+import com.onewelcome.core.util.Constants.DATA_KEY
 import com.onewelcome.core.util.Constants.MESSAGE_KEY
 import com.onewelcome.core.util.Constants.PROFILE_ID_KEY
+import com.onewelcome.core.util.Constants.STATUS_KEY
 import com.onewelcome.core.util.Constants.TIMESTAMP_KEY
 import com.onewelcome.core.util.Constants.TIME_TO_LIVE_SECONDS_KEY
 import com.onewelcome.core.util.Constants.TRANSACTION_ID_KEY
@@ -21,10 +23,15 @@ sealed class Screens(val route: String) {
   data object MobileAuthentication : Screens("mobile_authentication")
   data object MobileAuthenticationEnrollment : Screens("mobile_authentication_enrollment")
   data object MobileAuthenticationPushEnrollment : Screens("mobile_authentication_push_enrollment")
-  data object PushAuthentication : Screens("push_authentication")
   data object ChangePin : Screens("change_pin")
   data object Logout : Screens("logout")
   data object PinAuthenticationInput : Screens("pin_authentication_input_${Constants.FULLSCREEN_PAGE}")
   data object CreatePinInput : Screens("create_pin_input_${Constants.FULLSCREEN_PAGE}")
-  data object TransactionConfirmation : Screens("transaction_confirmation/{$TRANSACTION_ID_KEY}/{$MESSAGE_KEY}/{$PROFILE_ID_KEY}/{$TIMESTAMP_KEY}/{$TIME_TO_LIVE_SECONDS_KEY}")
+  data object TransactionConfirmation :
+    Screens("transaction_confirmation/{$TRANSACTION_ID_KEY}/{$MESSAGE_KEY}/{$PROFILE_ID_KEY}/{$TIMESTAMP_KEY}/{$TIME_TO_LIVE_SECONDS_KEY}")
+
+//  data object TransactionConfirmationSuccessful : Screens("transaction_confirmation_successful/{$STATUS_KEY}/{$DATA_KEY}}")
+  data object TransactionConfirmationResult : Screens("transaction_confirmation_result")
+//  data object TransactionConfirmationFailed :
+//    Screens("transaction_confirmation/{$TRANSACTION_ID_KEY}/{$MESSAGE_KEY}/{$PROFILE_ID_KEY}/{$TIMESTAMP_KEY}/{$TIME_TO_LIVE_SECONDS_KEY}")
 }
--- core/src/main/java/com/onewelcome/core/usecase/AuthenticateWithPushUseCase.kt
+++ core/src/main/java/com/onewelcome/core/usecase/AuthenticateWithPushUseCase.kt
@@ -1,21 +1,31 @@
 package com.onewelcome.core.usecase
 
+import com.github.michaelbull.result.Err
+import com.github.michaelbull.result.Ok
+import com.github.michaelbull.result.Result
 import com.onegini.mobile.sdk.android.handlers.OneginiMobileAuthenticationHandler
 import com.onegini.mobile.sdk.android.handlers.error.OneginiMobileAuthenticationError
 import com.onegini.mobile.sdk.android.model.entity.CustomInfo
 import com.onegini.mobile.sdk.android.model.entity.OneginiMobileAuthWithPushRequest
 import com.onewelcome.core.omisdk.facade.OmiSdkFacade
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class AuthenticateWithPushUseCase @Inject constructor(private val omiSdkFacade: OmiSdkFacade) {
+  private val _authenticationEvent = Channel<Result<CustomInfo?, OneginiMobileAuthenticationError>>(Channel.BUFFERED)
+  val authenticationEvent = _authenticationEvent.receiveAsFlow()
+
   fun execute(pushRequest: OneginiMobileAuthWithPushRequest) {
     omiSdkFacade.oneginiClient.getUserClient().handleMobileAuthWithPushRequest(pushRequest, object : OneginiMobileAuthenticationHandler {
       override fun onSuccess(customInfo: CustomInfo?) {
-        // onSuccess powinien informować TransactionConfirmationScreen że
+        _authenticationEvent.trySend(Ok(customInfo))
       }
 
       override fun onError(error: OneginiMobileAuthenticationError) {
-        // OnError powinien informować TransactionConfirmationScreen że coś poszło nietak
+        _authenticationEvent.trySend(Err(error))
       }
     })
   }
