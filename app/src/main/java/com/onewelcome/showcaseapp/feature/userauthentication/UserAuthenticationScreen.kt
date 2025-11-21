106e5666732899ac080b5de173afb32e0b8da27e
Aleksander Nowakowski <aleksander.nowakowski@onegini.com>
2025-11-21 15:23:53 +0100

    AOSA-45 Fix review remarks

--- app/src/main/java/com/onewelcome/showcaseapp/feature/userauthentication/UserAuthenticationScreen.kt
+++ app/src/main/java/com/onewelcome/showcaseapp/feature/userauthentication/UserAuthenticationScreen.kt
@@ -62,7 +62,7 @@ private fun Sections(onNavigateToSection: (String) -> Unit) {
 private fun getSections(): List<SectionItem> {
   return listOf(
     SectionItem(stringResource(R.string.section_title_pin_authentication), Screens.PinAuthentication),
-    SectionItem(stringResource(R.string.section_title_authenticator_settings), Screens.Authenticators)
+    SectionItem(stringResource(R.string.section_title_authenticators), Screens.Authenticators)
   )
 }
 
