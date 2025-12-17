6a95e2e2c1eefbb23f4df722387653b2b92fbed0
kasivissu <Kasi.Bannur@ltimindtree.com>
2025-12-17 10:42:04 +0530

    reverted changes as per comments

--- core/src/main/java/com/onewelcome/core/entity/HandlerType.kt
+++ core/src/main/java/com/onewelcome/core/entity/HandlerType.kt
@@ -6,11 +6,11 @@ import com.onewelcome.core.R
 //TODO: Uncomment handlers when working on given authentication
 enum class HandlerType(@StringRes val title: Int) {
   BROWSER_REGISTRATION(R.string.handler_browser_registration),
-  BIOMETRIC_AUTHENTICATION(R.string.handler_biometric_authentication),
-  //  CUSTOM_AUTHENTICATION,
+   BIOMETRIC_AUTHENTICATION(R.string.handler_biometric_authentication),
+//  CUSTOM_AUTHENTICATION,
   MOBILE_AUTH_WITH_PUSH(R.string.handler_mobile_auth_with_push),
   MOBILE_AUTH_WITH_PUSH_PIN(R.string.handler_mobile_auth_with_push_and_pin),
   //  MOBILE_AUTH_WITH_PUSH_BIOMETRIC,
-  //  MOBILE_AUTH_WITH_PUSH_CUSTOM,
+//  MOBILE_AUTH_WITH_PUSH_CUSTOM,
   MOBILE_AUTH_WITH_OTP(R.string.handler_mobile_auth_with_otp)
 }
