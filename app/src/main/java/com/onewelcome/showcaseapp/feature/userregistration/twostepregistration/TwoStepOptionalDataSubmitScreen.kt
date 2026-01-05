bb0c93c4fefb51831762779956301c92b80c8b34
PrabhatAgarwal04 <prabhat.prabhatagarwal1@gmail.com>
2026-01-05 17:11:18 +0530

    Added loading state on submitting the data

--- app/src/main/java/com/onewelcome/showcaseapp/feature/userregistration/twostepregistration/TwoStepOptionalDataSubmitScreen.kt
+++ app/src/main/java/com/onewelcome/showcaseapp/feature/userregistration/twostepregistration/TwoStepOptionalDataSubmitScreen.kt
@@ -1,6 +1,10 @@
 package com.onewelcome.showcaseapp.feature.userregistration.twostepregistration
 
+import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,6 +13,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
@@ -20,7 +25,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -42,6 +50,8 @@ fun TwoStepOptionalDataSubmitScreen(navController: NavController) {
         navController.getBackStackEntry(Screens.TwoStepRegistration.route)
     }
     val viewModel: TwoStepRegistrationViewModel = hiltViewModel(parentEntry)
+    val state = viewModel.uiState
+    Log.d("Prabhat","Recomposing    "+viewModel)
     TwoStepInputScreenContent(
         onNavigateBack = {
             viewModel.onEvent(UiEvent.CancelRegistration)
@@ -58,7 +68,26 @@ fun TwoStepOptionalDataSubmitScreen(navController: NavController) {
         },
         navigationEvents = viewModel.navigationEvents
     )
+    if (state.isOptionalDataSubmit) {
+        ShowProgressBar()
+    }
 
+}
+
+@Composable
+fun ShowProgressBar() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black.copy(alpha = 0.5f))
+            .clickable(enabled = true) {}
+            .pointerInput(Unit) {},
+        contentAlignment = Alignment.Center
+    ) {
+        CircularProgressIndicator(
+            color = Color.White
+        )
+    }
 }
 
 @Composable
